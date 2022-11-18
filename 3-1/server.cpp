29f6c5fb73fd21ddd28df6468850f60ea00ad8b5
RenesmeeCullen <934259048@qq.com>
2022-11-18 02:35:29 +0000

    64位协议，不知道超时重传什么毛病
    Signed-off-by: RenesmeeCullen <934259048@qq.com>

--- 3-1/server.cpp
+++ 3-1/server.cpp
@@ -7,17 +7,19 @@ using namespace std;
 
 
 const int MAXSIZE = 1024;//传输缓冲区最大长度
-const unsigned char SYN_1_ACK_0 = 0x4; //SYN = 1 ACK = 0
-const unsigned char SYN_1_ACK_1 = 0x5;//SYN = 0, ACK = 1
-const unsigned char SYN_0_ACK_1 = 0x1;
-const unsigned char FIN_1_ACK_1 = 0x3;//SYN = 1, ACK = 1
-const unsigned char FIN_1_ACK_0 = 0x2;//FIN = 1 ACK = 0
-const unsigned char END = 0x7;//结束标志，SYN=1,FIN=1,ACK=1
+u_short SYN_1_ACK_0 = 0x4; //SYN = 1 ACK = 0
+u_short SYN_1_ACK_1 = 0x5;//SYN = 0, ACK = 1
+u_short SYN_0_ACK_1 = 0x1;
+u_short FIN_1_ACK_1 = 0x3;//SYN = 1, ACK = 1
+u_short FIN_1_ACK_0 = 0x2;//FIN = 1 ACK = 0
+u_short END = 0x7;//结束标志，SYN=1,FIN=1,ACK=1
 double MAX_WAIT_TIME = 0.5 * CLOCKS_PER_SEC;
 long long head, tail, freq;
 
 
-
+void printsplit() {
+    cout << "--------------------------------------------------------------------------" << endl;
+}
 u_short cksum(u_short* mes, int size) {
     int count = (size + 1) / 2;
     u_short* buf = (u_short*)malloc(size + 1);
@@ -38,9 +40,9 @@ struct HEADER
 {
     u_short sum = 0;
     u_short datasize = 0;
-    unsigned char flag = 0;
+    u_short flag = 0;
 
-    unsigned char SEQ = 0;
+    u_short SEQ = 0;
 
     HEADER() {
         sum = 0;
@@ -75,8 +77,7 @@ int Connect(SOCKET& sockServ, SOCKADDR_IN& ClientAddr, int& ClientAddrLen)
     //发送第二次握手信息
     header.flag = SYN_1_ACK_1;
     header.sum = 0;
-    u_short temp = cksum((u_short*)&header, sizeof(header));
-    header.sum = temp;
+    header.sum = cksum((u_short*)&header, sizeof(header));
     memcpy(Buffer, &header, sizeof(header));
     if (sendto(sockServ, Buffer, sizeof(header), 0, (sockaddr*)&ClientAddr, ClientAddrLen) == -1)
     {
@@ -85,9 +86,9 @@ int Connect(SOCKET& sockServ, SOCKADDR_IN& ClientAddr, int& ClientAddrLen)
     
     
 
-    HEADER temp1;
-    memcpy(&temp1, Buffer, sizeof(header));
-    if (temp1.flag == SYN_1_ACK_1 && cksum((u_short*)&temp1, sizeof(temp1) == 0))
+    HEADER h;
+    memcpy(&h, Buffer, sizeof(header));
+    if (h.flag == SYN_1_ACK_1 && cksum((u_short*)&h, sizeof(h) == 0))
     {
         cout << "connection succeeded" << endl;
     }
@@ -118,7 +119,7 @@ int RecvMessage(SOCKET& sockServ, SOCKADDR_IN& ClientAddr, int& ClientAddrLen, c
             cout << "done" << endl;
             break;
         }
-        if (header.flag == unsigned char(0) && cksum((u_short*)Buffer, length - sizeof(header)))
+        if (header.flag == u_short(0) && cksum((u_short*)Buffer, length - sizeof(header)))
         {
             //to see if other data is recieved
             if (seq != int(header.SEQ))
@@ -126,22 +127,19 @@ int RecvMessage(SOCKET& sockServ, SOCKADDR_IN& ClientAddr, int& ClientAddrLen, c
                 //problems occurred. send back ACK
                 header.flag = SYN_0_ACK_1;
                 header.datasize = 0;
-                header.SEQ = (unsigned char)seq;
+                header.SEQ = (u_short)seq;
                 header.sum = 0;
-                u_short temp = cksum((u_short*)&header, sizeof(header));
-                header.sum = temp;
+                header.sum = cksum((u_short*)&header, sizeof(header));
                 memcpy(Buffer, &header, sizeof(header));
                 
                 sendto(sockServ, Buffer, sizeof(header), 0, (sockaddr*)&ClientAddr, ClientAddrLen);
+                printsplit();
                 cout << "Send to Client ACK:" << (int)header.flag << " SEQ:" << (int)header.SEQ << endl;
                 continue;//dump the data
             }
             seq = int(header.SEQ);
-            if (seq > 255)
-            {
-                seq = seq - 256;
-            }
-
+            
+            printsplit();
             cout << "Send message " << length - sizeof(header) << " B!Flag:" << int(header.flag) << " SEQ : " << int(header.SEQ) << " SUM:" << int(header.sum) << endl;
             char* temp = new char[length - sizeof(header)];
             memcpy(temp, Buffer + sizeof(header), length - sizeof(header));
@@ -152,26 +150,22 @@ int RecvMessage(SOCKET& sockServ, SOCKADDR_IN& ClientAddr, int& ClientAddrLen, c
             //return ACK
             header.flag = SYN_0_ACK_1;
             header.datasize = 0;
-            header.SEQ = (unsigned char)seq;
+            header.SEQ = (u_short)seq;
             header.sum = 0;
-            u_short temp1 = cksum((u_short*)&header, sizeof(header));
-            header.sum = temp1;
+            header.sum = cksum((u_short*)&header, sizeof(header));
             memcpy(Buffer, &header, sizeof(header));
             //sesend ACK
             sendto(sockServ, Buffer, sizeof(header), 0, (sockaddr*)&ClientAddr, ClientAddrLen);
+            printsplit();
             cout << "Send to Client ACK:" << (int)header.flag << " SEQ:" << (int)header.SEQ << endl;
             seq++;
-            if (seq > 255)
-            {
-                seq = seq - 256;
-            }
+            
         }
     }
     //发送OVER信息
     header.flag = END;
     header.sum = 0;
-    u_short temp = cksum((u_short*)&header, sizeof(header));
-    header.sum = temp;
+    header.sum = cksum((u_short*)&header, sizeof(header));
     memcpy(Buffer, &header, sizeof(header));
     if (sendto(sockServ, Buffer, sizeof(header), 0, (sockaddr*)&ClientAddr, ClientAddrLen) == -1)
     {
@@ -197,8 +191,7 @@ int disConnect(SOCKET& sockServ, SOCKADDR_IN& ClientAddr, int& ClientAddrLen)
     //发送第二次挥手信息
     header.flag = FIN_1_ACK_1;
     header.sum = 0;
-    u_short temp = cksum((u_short*)&header, sizeof(header));
-    header.sum = temp;
+    header.sum = cksum((u_short*)&header, sizeof(header));
     memcpy(Buffer, &header, sizeof(header));
     if (sendto(sockServ, Buffer, sizeof(header), 0, (sockaddr*)&ClientAddr, ClientAddrLen) == -1)
     {
@@ -218,8 +211,8 @@ int main()
     SOCKET server;
 
     server_addr.sin_family = AF_INET;//使用IPV4
-    server_addr.sin_port = htons(2456);
-    server_addr.sin_addr.s_addr = htonl(2130706433);
+    server_addr.sin_port = htons(8000);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     server = socket(AF_INET, SOCK_DGRAM, 0);
     bind(server, (SOCKADDR*)&server_addr, sizeof(server_addr));//绑定套接字，进入监听状态
