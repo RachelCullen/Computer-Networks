3ea7febc8a4e33ef5b85a7b3c7ab2821fb243401
RenesmeeCullen <934259048@qq.com>
2022-12-09 14:11:29 +0000

    多线程，但不能丢包
    Signed-off-by: RenesmeeCullen <934259048@qq.com>

--- 3-3/myserver.h
+++ 3-3/myserver.h
@@ -1,4 +1,4 @@
-#ifndef MYSERVER_H//¶¨ÒåÕâ¸öÍ·ÎÄ¼ş
+#ifndef MYSERVER_H//å®šä¹‰è¿™ä¸ªå¤´æ–‡ä»¶
 #define MYSERVER_H
 #include <iostream>
 #include <WINSOCK2.h>
@@ -12,7 +12,7 @@
 #pragma comment(lib, "ws2_32.lib")
 
 using namespace std;
-/*-----------------¸÷¸öÈ«¾Ö±äÁ¿¶¨Òå-----------------------------------*/
+/*-----------------å„ä¸ªå…¨å±€å˜é‡å®šä¹‰-----------------------------------*/
 const int MAXSIZE = 2048;
 SOCKADDR_IN server_addr;
 SOCKET server;
@@ -21,7 +21,7 @@ int ACK = 0;
 const int cwnd = 10;
 int addrlen = sizeof(server_addr);
 int exptdseqnum = 0;
-/*-------------------ÀàĞÍÖ¡Ê×²¿¶¨Òå-------------------------*/
+/*-------------------ç±»å‹å¸§é¦–éƒ¨å®šä¹‰-------------------------*/
 class Header {
 public:
     u_short datasize = 0;
@@ -45,15 +45,15 @@ public:
 
 };
 
-/*--------------------------------Êı¾İ°ü½á¹¹¶¨Òå-----------------------------------------------*/
+/*--------------------------------æ•°æ®åŒ…ç»“æ„å®šä¹‰-----------------------------------------------*/
 struct Packet {
-    Header header;//UDPÊ×²¿
-    char* Buffer;//Êı¾İÇø
+    Header header;//UDPé¦–éƒ¨
+    char* Buffer;//æ•°æ®åŒº
     Packet() {
         Buffer = new char[MAXSIZE + sizeof(header)]();
     }
 };
-/*----------------------------ËùÒªÓÃµÄº¯Êı--------------------------------------------------*/
+/*----------------------------æ‰€è¦ç”¨çš„å‡½æ•°--------------------------------------------------*/
 void printsplit();
 u_short cksum(u_short* buff, int size);
 long long time(long long head);
@@ -63,15 +63,15 @@ bool interact(SOCKET& sockServ, SOCKADDR_IN& ClientAddr, int& ClientAddrLen, str
 int receive(SOCKET& servsocket, SOCKADDR_IN& clientaddr, int& len, char* message);
 void start(int len);
 
-/*-----------------------¸÷º¯Êı¶¨Òå----------------------------------------------------------*/
+/*-----------------------å„å‡½æ•°å®šä¹‰----------------------------------------------------------*/
 void init() {
     WSADATA wsadata;
     WSAStartup(MAKEWORD(2, 2), &wsadata);
-    server_addr.sin_family = AF_INET;//Ê¹ÓÃIPV4
+    server_addr.sin_family = AF_INET;//ä½¿ç”¨IPV4
     server_addr.sin_port = htons(4000);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     server = socket(AF_INET, SOCK_DGRAM, 0);
-    if (bind(server, (SOCKADDR*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)//°ó¶¨Ì×½Ó×Ö£¬½øÈë¼àÌı×´Ì¬
+    if (bind(server, (SOCKADDR*)&server_addr, sizeof(server_addr)) == SOCKET_ERROR)//ç»‘å®šå¥—æ¥å­—ï¼Œè¿›å…¥ç›‘å¬çŠ¶æ€
         cout << "bind error!" << endl;
     else
         cout << "waiting client....." << endl;
@@ -87,14 +87,14 @@ u_short cksum(u_short* buff, int size) {
     memset(buf, 0, size + 1);
     memcpy(buf, buff, size);
     u_short sum = 0;
-    while (count--) {//¶ş½øÖÆ·´ÂëÇóºÍ
+    while (count--) {//äºŒè¿›åˆ¶åç æ±‚å’Œ
         sum += *buf++;
         if (sum & 0xFFFF0000) {
             sum &= 0xFFFF;
             sum++;
         }
     }
-    return ~(sum & 0xFFFF);//½á¹ûÈ¡·´
+    return ~(sum & 0xFFFF);//ç»“æœå–å
 }
 long long time(long long head) {
     long long tail, freq;
@@ -115,7 +115,7 @@ bool check_sign(Header header, u_char sign) {
 }
 
 bool interact(SOCKET& sockServ, SOCKADDR_IN& ClientAddr, int& ClientAddrLen, string type) {
-    //type ÊÇhello»ògoodbye
+    //type æ˜¯helloæˆ–goodbye
     int sendsign = 0, recvsign = 0;
     if (type == "hello") {
         sendsign = 0x5;
@@ -130,7 +130,7 @@ bool interact(SOCKET& sockServ, SOCKADDR_IN& ClientAddr, int& ClientAddrLen, str
     Header sendh;
     char* Buffer = new char[sizeof(recvh)];
     int res = 0;
-    //½ÓÊÕµÚÒ»´ÎÎÕÊÖĞÅÏ¢
+    //æ¥æ”¶ç¬¬ä¸€æ¬¡æ¡æ‰‹ä¿¡æ¯
     while (true) {
         res = recvfrom(sockServ, Buffer, sizeof(recvh), 0, (sockaddr*)&ClientAddr, &ClientAddrLen);
         if (res == -1) {
@@ -162,16 +162,19 @@ bool interact(SOCKET& sockServ, SOCKADDR_IN& ClientAddr, int& ClientAddrLen, str
 
 int receive(SOCKET& servsocket, SOCKADDR_IN& clientaddr, int& len, char* filedata) {
     Header sendh;
-    sendh.ack = exptdseqnum ; //×¼±¸ÊÕµÄ°üµÄackÖµÓ¦ÎªÆÚ´ıÖµ
+    sendh.ack = exptdseqnum ; //å‡†å¤‡æ”¶çš„åŒ…çš„ackå€¼åº”ä¸ºæœŸå¾…å€¼
     setSum(sendh); //mkpkt(expseq)
     Packet recvp;
     char* buf = new char[sizeof(sendh)];
     int offset = 0;
-    while (true) { //Ò»Ö±µÈ´ı½ÓÊÕÖĞ
+    while (true) { //ä¸€ç›´ç­‰å¾…æ¥æ”¶ä¸­
         int recvlen = recvfrom(servsocket, recvp.Buffer, sizeof(recvp.header) + MAXSIZE, 0, (sockaddr*)&clientaddr, &len);
-        if (recvlen) {//ÊÕµ½ÁË¶«Î÷
+        //int drop = rand() % 10;
+        //if (drop == 1)
+        //    continue;
+        if (recvlen) {//æ”¶åˆ°äº†ä¸œè¥¿
             memcpy(&recvp.header, recvp.Buffer, sizeof(recvp.header));
-            if ( exptdseqnum == recvp.header.seq && recvp.header.flag == 0x0 && cksum((u_short*)&recvp.Buffer, strlen(recvp.Buffer))) { //ÊÇÆÚ´ıµÄ°ü
+            if ( exptdseqnum == recvp.header.seq && recvp.header.flag == 0x0 && cksum((u_short*)&recvp.Buffer, strlen(recvp.Buffer))) { //æ˜¯æœŸå¾…çš„åŒ…
 
                 memcpy(filedata + offset, recvp.Buffer + sizeof(recvp.header), recvlen - sizeof(recvp.header));
                 offset += recvp.header.datasize;
@@ -179,8 +182,8 @@ int receive(SOCKET& servsocket, SOCKADDR_IN& clientaddr, int& len, char* filedat
                 sendh.setHeader(0, 0x1, exptdseqnum);
                 setSum(sendh); //mkpkt(expseq)
                 memcpy(buf, &sendh, sizeof(sendh));
-                sendto(servsocket, buf, sizeof(sendh), 0, (sockaddr*)&clientaddr, len); //sndpkt
-                cout << "[EXPECTED packet] send ack:" << (int)sendh.ack<<" ";
+                if(sendto(servsocket, buf, sizeof(sendh), 0, (sockaddr*)&clientaddr, len)) //sndpkt
+                    cout << "[EXPECTED packet] send ack:" << (int)sendh.ack<<" ";
                 sendh.show_header();
                 exptdseqnum++;
                 exptdseqnum %= 256;
@@ -199,15 +202,15 @@ int receive(SOCKET& servsocket, SOCKADDR_IN& clientaddr, int& len, char* filedat
                 break;
             }
             else {
-                //continue;//²»ÊÇÆÚ´ıµÄ°ü£¬ÂïÒ²²»¸ÉÒòÎª·¢ËÍ¶ËÒ²µÃºöÂÔ
-                //ÏÂÃæÇ³ÊÔÒ»ÏÂ·¢ËÍÉÏÒ»¸öµÄack
+                //continue;//ä¸æ˜¯æœŸå¾…çš„åŒ…ï¼Œå˜›ä¹Ÿä¸å¹²å› ä¸ºå‘é€ç«¯ä¹Ÿå¾—å¿½ç•¥
+                //ä¸‹é¢æµ…è¯•ä¸€ä¸‹å‘é€ä¸Šä¸€ä¸ªçš„ack
                 int lastack = exptdseqnum - 1;
                 sendh.ack = lastack;
-                sendh.setHeader(0, 0x0, lastack);//·¢ËÍÖØ¸´µÄack£¬flagÖÃ0ºÍ±ğµÄÇø·ÖÒ»ÏÂ
+                sendh.setHeader(0, 0x0, lastack);//å‘é€é‡å¤çš„ackï¼Œflagç½®0å’Œåˆ«çš„åŒºåˆ†ä¸€ä¸‹
                 setSum(sendh); //mkpkt(expseq)
                 memcpy(buf, &sendh, sizeof(sendh));
-                sendto(servsocket, buf, sizeof(sendh), 0, (sockaddr*)&clientaddr, len); //sndpkt
-                cout << "[UNEXPECTED packet] send last ack:" << (int)sendh.ack<<" ";
+                if(sendto(servsocket, buf, sizeof(sendh), 0, (sockaddr*)&clientaddr, len)) //sndpkt
+                    cout << "[UNEXPECTED packet] send last ack:" << (int)sendh.ack<<" ";
                 sendh.show_header();
 
             }
@@ -223,6 +226,7 @@ void start(int len) {
     char* myfile = new char[100000000];
 
     int namelen = receive(server, server_addr, len, fileName);
+    //cout << fileName << endl;
     int filelen = receive(server, server_addr, len, myfile);
 
     string file;
